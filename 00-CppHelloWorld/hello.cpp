ff9e3d29b1e8f01bafe2595941e224aebba78eae
FlorenciaQz <49354201+FlorenciaQz@users.noreply.github.com>
2020-05-07 20:00:54 -0300

    Create hello.cpp

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,12 @@
+/*
+    Trabajo Práctico 00: Hello, World!
+    Quiñonez Florencia Aylen
+    05/07/2020
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout <<"Hello, world!";
+    return 0;
+}
