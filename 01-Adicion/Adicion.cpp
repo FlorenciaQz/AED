7f7f0939f71056d9f0d35e60a1e8534978de5279
FlorenciaQz <49354201+FlorenciaQz@users.noreply.github.com>
2020-05-07 20:19:37 -0300

    Create Adicion.cpp

---
+++ 01-Adicion/Adicion.cpp
@@ -0,0 +1,16 @@
+/*
+Trabajo Práctico 01: Adición
+Florencia Quiñonez 
+05/07/2020
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout<<"Ingrese dos numeros enteros.\n";
+    int a;
+    std::cin>>a;
+    int b;
+    std::cin>>b;
+    std::cout<<a<<" + "<<b<<" = "<<a+b;
+}
