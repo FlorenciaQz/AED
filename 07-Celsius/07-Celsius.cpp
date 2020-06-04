c50c251733bf8a1a20da1c629dcc4b73f392cf27
FlorenciaQz <49354201+FlorenciaQz@users.noreply.github.com>
2020-06-04 00:57:28 -0300

    Create 07-Celsius.cpp

---
+++ 07-Celsius/07-Celsius.cpp
@@ -0,0 +1,37 @@
+/*
+    Trabajo Práctico 07 : Celsius
+    Quiñonez Florencia Aylen
+    06/03/2020
+*/
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+double Celsius(double);
+bool AreNear(double, double, double);
+
+
+int main()
+{
+    assert(AreNear(Celsius(-20), -28.889, 0.001));
+    assert(AreNear(Celsius(-16), -26.667, 0.001));
+    assert(AreNear(Celsius(0), -17.778, 0.001));
+    assert(AreNear(Celsius(7), -13.889, 0.001));
+    assert(AreNear(Celsius(19), -7.222, 0.001));
+    assert(AreNear(Celsius(32), 0, 0.001));
+    assert(AreNear(Celsius(38), 3.333, 0.001));
+    assert(AreNear(Celsius(39), 3.888, 0.001));    
+
+}
+
+double Celsius(double fahrenheit) {
+
+    return (5.0 * (fahrenheit - 32)) / 9.0;
+
+}
+
+bool AreNear(double a, double b, double tol) {
+
+    return abs(a - b) <= tol;
+}
