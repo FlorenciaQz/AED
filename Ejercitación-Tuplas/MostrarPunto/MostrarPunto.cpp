9262cbb605cffb1c108f66cc57864a46540f7a26
FlorenciaQz <49354201+FlorenciaQz@users.noreply.github.com>
2020-09-03 12:21:17 -0300

    Update MostrarPunto.cpp

--- Ejercitación-Tuplas/MostrarPunto/MostrarPunto.cpp
+++ Ejercitación-Tuplas/MostrarPunto/MostrarPunto.cpp
@@ -1,8 +1,19 @@
 
 /* Enviar cualquier punto a la salida estandar 
 
-Hipótesis: El usuario deberá ingresar uno a uno los componentes de la estructura Punto
-por consola, y ambos serán mostrados en conjunto por consola.
+Hipótesis:
+
+Crear una estructura Punto compuesta por dos números reales y mostar el punto por consola.
+ 
+Los puntos se mostrarán mediante la función MostrarPunto (void) que a su vez obtendrá la información de la función ObtenerPunto (void) que pedirá un input.
+
+La forma de mostrar el punto será concatenando sus componentes en un cout. 
+
+cout << "(" << p.x << ";" << p.y << ")"; 
+
+Dominio del problema.
+
+MostrarPunto: Punto --> [ ] --> Sigma*
 
 */
 
